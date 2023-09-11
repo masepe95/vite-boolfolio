1e407fbb4c9ddc4a0c44cff656f95b33c0f11a5f
masepe95 <marcosepe10@gmail.com>
2023-09-11 20:27:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<script>
+
+import axios from 'axios';
+
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+
+  components: {
+
+    ProjectCard
+
+  },
+
+  data() {
+
+    return {
+
+      endpoint: 'http://127.0.0.1:8000/api/projects',
+
+      projects: [],
+
+    }
+
+  },
+
+  methods: {
+
+    fetchProjects() {
+
+      axios.get(this.endpoint).then((res) => {
+
+        console.log(res.data);
+
+
+
+        this.projects = res.data.projects.data
+
+
+
+        console.log(this.projects);
+
+      })
+
+    }
+
+  },
+
+  created() {
+
+    this.fetchProjects();
+
+  }
+
+}
+
+</script>
+
+
+
+
+<template>
+  <div class="container">
+
+    <h1 class="text-danger">Projects</h1>
+
+    <ul class="mt-5 list-unstyled">
+
+      <li class="my-3" :key="project.id" v-for="project in projects">
+
+        <AppCard :project="project" />
+
+      </li>
+
+    </ul>
+
+  </div>
+</template>
+
+
+
+
+<style></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,45 @@
+<script>
+
+export default {
+
+    props: {
+
+        project: Object
+
+    },
+
+    data() {
+
+        return {
+
+
+
+        }
+
+    },
+
+    methods: {
+
+
+
+    }
+
+}
+
+</script>
+
+
+
+
+<template>
+    <div class="card p-3">
+
+        <h1>{{ project.title }}</h1>
+
+    </div>
+</template>
+
+
+
+
+<style scoped></style>
