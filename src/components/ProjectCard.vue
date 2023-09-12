3953d3296079d750661a0e3e49a3876dc1bd2b4a
masepe95 <marcosepe10@gmail.com>
2023-09-12 18:41:45 +0200

    card style

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -32,10 +32,23 @@ export default {
 
 
 <template>
-    <div class="card p-3">
-
-        <h1>{{ project.title }}</h1>
-
+    <div class="card p-5 bg-dark text-white">
+        <div class="card-header rounded border-0 mb-4 d-flex justify-content-between align-content-center ">
+            <h2 class="m-0 d-flex align-items-center">
+                {{ project.title }}
+            </h2>
+        </div>
+        <div class="card-body">
+            <p class="">
+                {{ project.description }}
+            </p>
+        </div>
+        <div class="card-footer d-flex justify-content-between mt-3 align-items-center border-0 bg-dark">
+            <div>
+                Created: {{ project.created_at }} <br>
+                Last Modified at: {{ project.updated_at }}
+            </div>
+        </div>
     </div>
 </template>
 
