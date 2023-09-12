59b658939f4e690ec9defc4cfcf14cbc2e147734
masepe95 <marcosepe10@gmail.com>
2023-09-12 18:34:42 +0200

    fetch fix

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,40 @@
 <script>
-
 import axios from 'axios';
-
 import ProjectCard from './components/ProjectCard.vue';
-
 export default {
-
   components: {
-
     ProjectCard
-
   },
-
   data() {
-
     return {
-
       endpoint: 'http://127.0.0.1:8000/api/projects',
-
       projects: [],
-
     }
-
   },
-
   methods: {
-
     fetchProjects() {
-
       axios.get(this.endpoint).then((res) => {
-
         console.log(res.data);
-
-
-
-        this.projects = res.data.projects.data
-
-
-
+        this.projects = res.data.data
         console.log(this.projects);
-
       })
-
     }
-
   },
-
   created() {
-
     this.fetchProjects();
-
   }
-
 }
-
 </script>
-
-
-
 
 <template>
   <div class="container">
-
-    <h1 class="text-success">Projects</h1>
-
+    <h1 class="text-primary">Projects</h1>
     <ul class="mt-5 list-unstyled">
-
       <li class="my-3" :key="project.id" v-for="project in projects">
-
-        <AppCard :project="project" />
-
+        <ProjectCard :project="project" />
       </li>
-
     </ul>
-
   </div>
 </template>
-
-
-
 
 <style></style>
